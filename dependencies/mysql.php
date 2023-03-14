e2a4d3bb7054aaa41141fae190180c8553d268e4
PalmarHealer <93807726+PalmarHealer@users.noreply.github.com>
2023-03-14 20:10:11 +0100

    Moved more MySQL stuff now almost everything that does stuff with the database is in here

--- dependencies/mysql.php
+++ dependencies/mysql.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function update_or_insert_lesson($type, $pdo, $id, $date_type, $new_date, $new_name, $new_description, $new_location, $new_time, $new_notes, $new_assigned_user_id) {
 
     try {
@@ -71,6 +70,32 @@ function GetLesson($day, $time, $room, $info, $pdo) {
     }
 }
 
+function GetLessonByID($id, $info, $pdo) {
+
+
+    $lessons = $pdo->prepare("SELECT * FROM angebot WHERE id = ?");
+    $lessons->execute(array($id));
+
+    while($sl = $lessons->fetch()) {
+
+        if (!isset($sl)) {
+            continue;
+        }
+        if ($info == "name") {
+            return $sl['name'];
+        }
+        if ($info == "description") {
+            return $sl['description'];
+        }
+        if ($info == "userid") {
+            return $sl['assigned_user_id'];
+        }
+        if ($info == "available") {
+            return false;
+        }
+
+    }
+}
 
 function GetInfomationOfUser($UserID, $InfomationType, $pdo) {
     if (!is_numeric($UserID)) {
@@ -95,7 +120,7 @@ function GetInfomationOfUser($UserID, $InfomationType, $pdo) {
 
 }
 
-function updateUser($id, $vorname, $nachname, $email, $permission_level, $pdo) {
+function UpdateUser($id, $vorname, $nachname, $email, $permission_level, $pdo) {
     $statement = $pdo->prepare("UPDATE users SET vorname = :vorname, nachname = :nachname, email = :email, permission_level = :permission_level WHERE id = :id");
     $statement->execute(array(
         'id' => $id,
@@ -107,3 +132,133 @@ function updateUser($id, $vorname, $nachname, $email, $permission_level, $pdo) {
     return $statement->rowCount(); // gibt zurück, wie viele Zeilen aktualisiert wurden, danke ChatGPT
 }
 
+function DeleteLesson($lessonid, $pdo) {
+    try {
+        $delete_lesson = $pdo->prepare("DELETE FROM angebot WHERE id = ?");
+        $delete_lesson->execute(array($lessonid));
+        return true;
+    } catch (PDOException $e) {
+        return "Fehler beim Löschen: " . $e->getMessage();
+    }
+}
+
+function GetAllLessonsFromUserAndPrintThem($userid, $limit, $room_names, $times, $pdo) {
+
+    $lessons = $pdo->prepare("SELECT * FROM angebot WHERE assigned_user_id = ? ORDER BY date ASC");
+    $lessons->execute(array($userid));
+    $counter = 1;
+    $pdo = null;
+
+
+    while($sl = $lessons->fetch()) {
+
+        if (isset($sl['date'])) {
+            if (date("Y-m-d",time()) > $sl['date'] OR $counter > $limit) {
+                continue;
+            }
+        }
+
+        $counter += 1;
+
+        if ($sl['date_type'] == "2") {
+            $date1 = $sl['date'];
+            $single_date1 = explode("-", $date1);
+            $date_fomatted = $single_date1[2] . "." . $single_date1[1] . "." . $single_date1[0];
+        } else {
+            $date_day = $sl['date_repeating'];
+            if ($date_day == "1") {
+                $date_fomatted = "Jeden Montag";
+            } elseif ($date_day == "2") {
+                $date_fomatted = "Jeden Dienstag";
+            } elseif ($date_day == "3") {
+                $date_fomatted = "Jeden Mittwoch";
+            } elseif ($date_day == "4") {
+                $date_fomatted = "Jeden Donnerstag";
+            } elseif ($date_day == "5") {
+                $date_fomatted = "Jeden Freitag";
+            } else {
+                $date_fomatted = "Fehler beim Laden des Datums";
+            }
+        }
+
+        echo '<tr>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">
+										  </td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $sl['name'] . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $sl['description'] . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $room_names[$sl['location']] . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $times[$sl['time']] . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $date_fomatted . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $sl['notes'] . '</td>
+										
+										  <td><button class="btn btn-sm dropdown-toggle more-horizontal" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												<span class="text-muted sr-only">Action</span>
+											  </button>
+											  <div class="dropdown-menu dropdown-menu-right">
+												<a class="dropdown-item" href="../lessons/details/?id=' . $sl['id'] . '">Edit</a>
+												<a class="dropdown-item" href="../lessons/details/?remove_lesson_with_id=' . $sl['id'] . '">Remove</a>
+											  </div>
+										  </td>
+										  </tr>';
+    }
+
+}
+
+function GetAllLessons($room_names, $times, $pdo) {
+    $lessons = $pdo->prepare("SELECT * FROM angebot");
+    $lessons->execute();
+    while($sl = $lessons->fetch()) {
+
+        if ($sl['date_type'] == "2") {
+            $date1 = $sl['date'];
+            $single_date1 = explode("-", $date1);
+            $date_fomatted = $single_date1[2] . "." . $single_date1[1] . "." . $single_date1[0];
+        } else {
+            $date_day = $sl['date_repeating'];
+            if ($date_day == "1") {
+                $date_fomatted = "Jeden Montag";
+            } elseif ($date_day == "2") {
+                $date_fomatted = "Jeden Dienstag";
+            } elseif ($date_day == "3") {
+                $date_fomatted = "Jeden Mittwoch";
+            } elseif ($date_day == "4") {
+                $date_fomatted = "Jeden Donnerstag";
+            } elseif ($date_day == "5") {
+                $date_fomatted = "Jeden Freitag";
+            } else {
+                $date_fomatted = "Fehler beim Laden des Datums";
+            }
+        }
+
+
+
+        $creator_id = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+        $creator_id->execute(array($sl['assigned_user_id']));
+        while ($creator_name = $creator_id->fetch()) {
+            $creator_fomatted = $creator_name['vorname'] . " " . $creator_name['nachname'];
+        }
+        echo '<tr>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">
+										  </td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $sl['name'] . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $sl['description'] . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $room_names[$sl['location']] . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $times[$sl['time']] . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $date_fomatted . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $creator_fomatted . '</td>
+										  <td class="pointer" onClick="window.location=\'./../lessons/details/?id=' . $sl['id'] . '\';">' . $sl['notes'] . '</td>
+										
+										  <td><button class="btn btn-sm dropdown-toggle more-horizontal" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												<span class="text-muted sr-only">Action</span>
+											  </button>
+											  <div class="dropdown-menu dropdown-menu-right">
+												<a class="dropdown-item" href="./details/?id=' . $sl['id'] . '">Edit</a>
+												<a class="dropdown-item" href="./details/?remove_lesson_with_id=' . $sl['id'] . '">Remove</a>
+											  </div>
+										  </td>
+										  </tr>';
+
+    }
+
+
+}
