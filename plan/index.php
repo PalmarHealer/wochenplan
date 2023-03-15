2e4959aad86143507ace7136f2481bd3a14d31a8
PalmarHealer <93807726+PalmarHealer@users.noreply.github.com>
2023-03-15 03:21:49 +0100

    Plan Made that the plan does automatically reload his content.

--- plan/index.php
+++ plan/index.php
@@ -42,151 +42,40 @@ if (!isset($_GET["date"])) {
   </head>
   <body class="full">
 
-  <table class="full tg">
-      <colgroup>
-          <col class="piece"/>
-          <col/>
-          <col/>
-          <col/>
-          <col/>
-          <col/>
-          <col/>
-          <col/>
-          <col/>
-          <col/>
-      </colgroup>
-      <thead>
-      <tr class="small_piece center">
-          <th class="color-3 no_border">
-              <b class="white_text modt">
-                  <?php
-                        $weekday = (new DateTime($current_day))->format('N');
-                        echo $weekday_names[$weekday];
-                    ?>
-                </b>
-            </th>
-            <th class="color-1" colspan="4"></th>
-            <th class="color-3" colspan="5"></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr class="name-badge center">
-            <td class="color-3 no_border"><b class="white_text heading"><?php echo date('d.m.Y', strtotime($current_day)); ?></b></td>
-            <td class="color-2 no_border"><b class="bold">Raum 1</b></td>
-            <td class="color-2 no_border"><b class="bold">Raum 2</b></td>
-            <td class="color-2 no_border"><b class="bold">Raum 3 (HS)</b></td>
-            <td class="color-2 no_border"><b class="bold">Raum 4 (RS)</b></td>
-            <td class="color-2 no_border">Gesprächsraum</td>
-            <td class="color-2 no_border">SZ/Praxisber.</td>
-            <td class="color-2 no_border">Sport</td>
-            <td class="color-2 no_border">Ausflug</td>
-            <td class="color-1 no_border">Freiarbeit</td>
-        </tr>
-        <tr class="small_piece">
-            <td class="color-1 no_border">
-                8:00 – 9:00<br />
-                <b class="bold">Morgenband</b>
-            </td>
-            <td class="color-2 no_border center" colspan="8"><p>Ankommen</p></td>
-            <td class="color-1"></td>
-        </tr>
-        <tr class="piece">
-            <td class="color-1 no_border">
-                9:00 - 10:00<br />
-                <b class="bold">Morgenband</b>
-            </td>
-            <td class="color-2 db_text"><?php PrintLessonToPlan($current_day, 1, 1, $pdo); ?></td>
-            <td class="color-2 db_text"><?php PrintLessonToPlan($current_day, 1, 2, $pdo); ?></td>
-            <td class="color-2 db_text"><?php PrintLessonToPlan($current_day, 1, 3, $pdo); ?></td>
-            <td class="color-2 db_text"><?php PrintLessonToPlan($current_day, 1, 4, $pdo); ?></td>
-            <td class="color-2 db_text"><?php PrintLessonToPlan($current_day, 1, 5, $pdo); ?></td>
-            <td class="color-2 db_text"><?php PrintLessonToPlan($current_day, 1, 6, $pdo); ?></td>
-            <td class="color-2 db_text" rowspan="3"><?php PrintLessonToPlan($current_day, 1, 7, $pdo); ?></td>
-            <td class="color-2 db_text" rowspan="3"><?php PrintLessonToPlan($current_day, 1, 8, $pdo); ?></td>
-            <td class="color-1 db_text"></td>
-        </tr>
-        <tr class="piece">
-            <td class="color-1 no_border">
-                10:00 – 10:30<br/>
-                <b class="bold">Morgenkreise</b>
-            </td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-1"></td>
-        </tr>
-        <tr>
-            <td class="color-1 no_border">
-                10:30 – 12:00<br/>
-                <b class="bold">Großes Band</b>
-            </td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-1"></td>
-        </tr>
-        <tr class="breakfast">
-            <td class="color-1 no_border">
-                12:00 – 13:00<br/>
-                <b class="bold">Mittagspause</b>
-            </td>
-            <td class="color-4 no_border" colspan="8"><b class="bold">Mittagessen</b> </td>
-            <td class="color-1"></td>
-        </tr>
-        <tr>
-            <td class="color-1 no_border">
-                13:00 – 14:30<br/>
-                <b class="bold">Nachmittags-<br/>
-                    band</b>
-            </td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-1"></td>
-        </tr>
-        <tr class="small_piece">
-            <td class="color-1 no_border" rowspan="2">
-                14:30 – 15:00<br/>
-                <b class="bold">Putzen</b>
-            </td>
-            <td class="color-5" colspan="8"></td>
-            <td class="color-1"></td>
-        </tr>
-        <tr class="small_piece">
-            <td class="color-5" colspan="2"></td>
-            <td class="color-5" colspan="3"></td>
-            <td class="color-5" colspan="3"></td>
-            <td class="color-1"></td>
-        </tr>
-        <tr class="piece">
-            <td class="color-1 no_border">
-                15:00 – 16:00<br/>
-                <b class="bold">Spätes Band</b>
-            </td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-2"></td>
-            <td class="color-1"></td>
-        </tr>
-        </tbody>
-    </table>
-	
-	
+
+
+
+
+
+
   </body>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script>
+
+      $(document).ready(function() {
+          // Lade Daten 2 Sekunden nach dem Laden der Seite
+          setTimeout(reloadData, 2000);
+
+          var intval = <?php echo ($_GET['interval'] ?? '1'); ?>;
+      });
+
+      function reloadData() {
+          $.ajax({
+              url: "./reload.php",
+              type: "POST",
+              data: { date: "<?php echo $current_day; ?>" },
+              cache: false,
+              success: function(data) {
+                  console.log("Data reloaded");
+                  $(".full").html(data);
+
+                  var intval = <?php echo ($_GET['interval'] ?? '1'); ?>;
+                  setInterval(reloadData, intval * 1000);
+              }
+          });
+      }
+
+
+  </script>
 </html>
