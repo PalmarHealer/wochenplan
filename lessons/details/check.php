d90019cba8e0495f730f1ee9cc7ffd68b439efe6
PalmarHealer <93807726+PalmarHealer@users.noreply.github.com>
2023-04-15 21:25:34 +0200

    small simplification

--- lessons/details/check.php
+++ lessons/details/check.php
@@ -16,12 +16,11 @@ if (str_contains($date, "/")) {
 if (!GetLessonInfo($newDate, $time, $location, "available", $pdo)) {
     echo '<div class="alert alert-success center" role="alert">';
     echo '<span class="fe fe-alert-octagon fe-16 mr-2"></span>Dieser Slot ist noch frei.';
-    echo '</div>';
 } else {
     echo '<div class="alert alert-danger center" role="alert">';
-    echo '<span class="fe fe-minus-circle fe-16 mr-2"></span>Leider ist dieser Slot schon belegt von <b>' . GetLessonInfo($newDate, $time, $location, "name", $pdo) . "</b> bitte sprich mit <b>" . GetUserByID(GetLessonInfo($date, $time, $location, "userid", $pdo), "vorname", $pdo) . "</b>";
-    echo '</div>';
+    echo '<span class="fe fe-minus-circle fe-16 mr-2"></span>Leider ist dieser Slot schon belegt von <b>' . replacePlaceholders(GetLessonInfo($newDate, $time, $location, "name", $pdo)) . "</b> bitte sprich mit <b>" . GetUserByID(GetLessonInfo($date, $time, $location, "userid", $pdo), "vorname", $pdo) . "</b>";
 }
+echo '</div>';
 $pdo = null;
 ?>
 
