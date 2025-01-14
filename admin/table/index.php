66573b4bb93a8c80065ae092a11f87d3fd456e76
palmarhealer <myoserhd111@gmail.com>
2025-01-14 11:35:34 +0100

    Update: Style and Types Updated styles, added a new stylesheet link and changed an invisible input type

--- admin/table/index.php
+++ admin/table/index.php
@@ -43,6 +43,7 @@ if (isset($_GET['load']) and !($_GET['load'] == '')) {
     <link rel="stylesheet" href="<?php echo $relative_path; ?>/css/customstyle.css?version=<?php echo $version; ?>">
     <!-- Custom Site CSS -->
     <link rel="stylesheet" href="<?php echo $relative_path; ?>/css/customTableStyle.css?version=<?php echo $version; ?>">
+    <link rel="stylesheet" href="<?php echo $relative_path; ?>/css/quill.snow.css?version=<?php echo $version; ?>">
 
 </head>
 
@@ -60,17 +61,17 @@ if (isset($_GET['load']) and !($_GET['load'] == '')) {
                     <div id="tableContainer">
                         <div class="card-header display-contents">
                             <form class="display-flex">
-                                <div class="form-group margin-10">
-                                    <label class="col-form-label" for="label">Text:</label>
-                                    <div class="input-group mb-3">
+                                <div class="form-group margin-10 flex-1 position-relative">
+                                    <label class="col-form-label" for="name">Text:</label>
+                                    <div class="form-group mb-3">
 
-                                        <input placeholder="N/A" type="text" id="label" class="form-control">
-                                        <div class="input-group-append">
-                                            <span class="input-group-text fe fe-24 fe-file-text"></span>
+                                        <div type="text" id="nameEditor" class="form-control">
+                                            <?php echo ($lesson_details['name'] ?? '');?>
                                         </div>
+                                        <input name="name" type="text" id="name" class="hidden-input form-control">
                                     </div>
                                 </div>
-                                <div class="form-group margin-10">
+                                <div class="form-group margin-10 flex-1">
                                     <label class="col-form-label" for="time">Zeit ID:</label>
                                     <div class="input-group mb-3">
 
@@ -93,7 +94,7 @@ if (isset($_GET['load']) and !($_GET['load'] == '')) {
                                         </div>
                                     </div>
                                 </div>
-                                <div class="form-group margin-10">
+                                <div class="form-group margin-10 flex-1">
                                     <label class="col-form-label" for="room">Raum ID:</label>
                                     <div class="input-group mb-3">
 
@@ -117,7 +118,7 @@ if (isset($_GET['load']) and !($_GET['load'] == '')) {
                                         </div>
                                     </div>
                                 </div>
-                                <div class="form-group margin-10">
+                                <div class="form-group margin-10 flex-1">
                                     <label class="col-form-label" for="color">Farbe:</label>
                                     <div class="input-group mb-3">
                                         <select id="color" name="color" class="form-control" onchange="dyeCells()">
@@ -139,7 +140,7 @@ if (isset($_GET['load']) and !($_GET['load'] == '')) {
                                         </div>
                                     </div>
                                 </div>
-                                <div class="form-group margin-10">
+                                <div class="form-group margin-10 flex-1">
                                     <label class="col-form-label" for="name">Name der Plan Version:</label>
                                     <div class="input-group mb-3">
 
@@ -192,6 +193,47 @@ if (isset($_GET['load']) and !($_GET['load'] == '')) {
 <script src="<?php echo $relative_path; ?>/js/config.js?version=<?php echo $version; ?>"></script>
 <script src="<?php echo $relative_path; ?>/js/apps.js?version=<?php echo $version; ?>"></script>
 <script src="<?php echo $relative_path; ?>/js/table.custom.js?version=<?php echo $version; ?>"></script>
+<script src="<?php echo $relative_path; ?>/js/quill.min.js?version=<?php echo $version; ?>"></script>
 <script src="<?php echo $relative_path; ?>/js/customjavascript.js?version=<?php echo $version; ?>"></script>
+<script>
+    $(document).ready(function() {
+
+        $('.ql-toolbar').on('mousedown mouseup click', function() {
+            $('#name').val($('.ql-editor').html()).change();
+            updateText();
+        });
+
+        $('.ql-editor').on('keyup keydown input change', function() {
+            let htmlContent = $(this).html();
+            htmlContent = htmlContent.replace(/<span class="ql-cursor">\s*﻿?\s*<\/span>/g, '');
+            let tempDiv = $('<div>').html(htmlContent);
+            tempDiv.find('*').not('br').each(function() {
+                if ($(this).is(':empty')) {
+                    $(this).remove();
+                }
+            });
+            $('#name').val($(this).html()).change();
+            updateText();
+        });
+
+
+    });
+    const toolbarOptions = {
+        container: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['clean']
+        ],
+
+    }
+
+    const quill = new Quill('#nameEditor', {
+        modules: {
+            "toolbar": toolbarOptions
+        },
+        placeholder: 'N/A',
+        theme: 'snow',
+    });
+
+</script>
 </body>
 </html>
